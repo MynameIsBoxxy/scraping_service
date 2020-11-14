5e9db583ead019e7953b0b026463dedf69bb613a
Alexandr <royscheider87@gmail.com>
2020-11-14 11:26:39 +0300

    add models

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import check_password
 
+from scraping.models import City, Language
+
 User = get_user_model()
+
 
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
@@ -23,10 +26,12 @@ class UserLoginForm(forms.Form):
                 raise forms.ValidationError('Данный аккаунт отключен')
         return super(UserLoginForm, self).clean(*args, **kwargs)
 
+
 class UserRegistrationFor(forms.ModelForm):
     email = forms.CharField(label='Введите email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Введите пароль еще раз', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Введите пароль еще раз',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
@@ -37,3 +42,21 @@ class UserRegistrationFor(forms.ModelForm):
         if data['password'] != data['password2']:
             raise forms.ValidationError('Пароли не совпадают')
         return data['password2']
+
+class UserUpdateForm(forms.Form):
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(), to_field_name="slug", required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Город'
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(), to_field_name="slug", required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Специальность'
+    )
+    send_email = forms.BooleanField(required=False, widget=forms.CheckboxInput,
+                                    label='Получать рассылку?')
+
+    class Meta:
+        model = User
+        fields = ('city', 'language', 'send_email')
--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +130,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.MyUser'
+
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+
